800d603816af68e5e4ccb96a5bb9f1f34ab29f49
AbdAllah-DraGoN <abdallahdragon72@gmail.com>
2025-04-05 00:02:08 +0200

    upload files

---
+++ custom-sections.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: Custom sections Shortcodes
+ * Description: Shortcode to display posts in a custom flexbox layout.
+ * Version: 1.0
+ * Author: abdullah mohamed
+ */
+
+function custom_posts_shortcode($atts) {
+    // shortcode attributes
+    $atts = shortcode_atts(array(
+        'count' => 5, // default number of posts to display
+    ), $atts, 'custom_posts');
+
+    // sgt posts
+    $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => intval($atts['count']),
+    );
+
+    $query = new WP_Query($args);
+    $output = '';
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $image = get_the_post_thumbnail_url(get_the_ID(), 'medium');
+            $title = get_the_title();
+            $excerpt = get_the_excerpt();
+
+            $output .= '
+                <div style="display: flex; justify-content: space-evenly; padding: 10px 40px; align-items: center; margin-bottom: 20px; border-bottom: 1px solid #ccc;">
+                    <img src="' . esc_url($image) . '" alt="' . esc_attr($title) . '" width="200px" />
+                    <div style="max-width: 600px;">
+                        <h1 style="margin: 0 0 10px;">' . esc_html($title) . '</h1>
+                        <p>' . esc_html($excerpt) . '</p>
+                    </div>
+                </div>';
+        }
+        wp_reset_postdata();
+    } else {
+        $output = '<p>No posts found.</p>';
+    }
+
+    return $output;
+}
+add_shortcode('custom_posts', 'custom_posts_shortcode');
