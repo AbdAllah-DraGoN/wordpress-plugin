4d9ea3f64295daa872f0d7cbf6ae165065dd75b0
AbdAllah-DraGoN <abdallahdragon72@gmail.com>
2025-04-05 00:42:31 +0200

    update 3

--- custom-sections.php
+++ custom-sections.php
@@ -1,33 +1,34 @@
 <?php
 /**
- * Plugin Name: Custom sections Shortcodes
+ * Plugin Name: Custom Sections Shortcodes
  * Description: Shortcode to display posts in a custom flexbox layout.
  * Version: 1.0
- * Author: abdullah mohamed
+ * Author: Abdullah Mohamed
  */
 
- // add assets
+// Load CSS & JS assets
 function custom_posts_enqueue_assets() {
     wp_enqueue_style('custom-posts-style', plugin_dir_url(__FILE__) . 'assets/style.css');
     wp_enqueue_script('custom-posts-script', plugin_dir_url(__FILE__) . 'assets/script.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'custom_posts_enqueue_assets');
 
-// to display posts services
+// Shortcode to display posts
 function custom_posts_shortcode($atts) {
-    // shortcode attributes
     $atts = shortcode_atts(array(
-        'count' => 5, // default number of posts to display
-        'category'=> 'services',
+        'count' => 5,
+        'category' => '',
     ), $atts, 'custom_posts');
 
-    // get posts
     $args = array(
         'post_type' => 'post',
         'posts_per_page' => intval($atts['count']),
-        'post_category'=> strval($atts['category']),
     );
-    // WP_Query => connect to the database and get the posts
+
+    if (!empty($atts['category'])) {
+        $args['category_name'] = sanitize_text_field($atts['category']);
+    }
+
     $query = new WP_Query($args);
     $output = '';
 
@@ -37,19 +38,21 @@ function custom_posts_shortcode($atts) {
             $image = get_the_post_thumbnail_url(get_the_ID(), 'medium');
             $title = get_the_title();
             $excerpt = get_the_excerpt();
+            $permalink = get_permalink();
 
             $output .= '
-                <div style="display: flex; justify-content: space-evenly; padding: 10px 40px; align-items: center; margin-bottom: 20px; border-bottom: 1px solid #ccc;">
-                    <img src="' . esc_url($image) . '" alt="' . esc_attr($title) . '" width="200px" />
-                    <div style="max-width: 600px;">
-                        <h1 style="margin: 0 0 10px;">' . esc_html($title) . '</h1>
-                        <p>' . esc_html($excerpt) . '</p>
+                <div class="custom-post-box">
+                    <img src="' . esc_url($image) . '" alt="' . esc_attr($title) . '" class="custom-post-img" />
+                    <div class="custom-post-content">
+                        <h1 class="custom-post-title">' . esc_html($title) . '</h1>
+                        <p class="custom-post-excerpt">' . esc_html($excerpt) . '</p>
+                        <a href="' . esc_url($permalink) . '" class="custom-post-readmore">اقرأ المزيد</a>
                     </div>
                 </div>';
         }
         wp_reset_postdata();
     } else {
-        $output = '<p>No posts found.</p>';
+        $output = '<p>لم يتم العثور على مقالات.</p>';
     }
 
     return $output;
